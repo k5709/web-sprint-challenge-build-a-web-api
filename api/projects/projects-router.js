e7ff0d96da28200900cd1601405b817a51be26e0
Kristian Rosales <r.kristian40@gmail.com>
2023-04-04 15:00:02 -0500

    finish tommorow or later

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -29,16 +29,57 @@ router.post("/", (req, res) => {
   const { name, description, completed } = req.body;
 
   if (!name || !description || !completed) {
-    return res.status(400);
-  }
-  Projects.insert({ name, description, completed }).then((projects) => {
-    res.json(projects);
-  });
+    res.status(400).end();
+  } else
+    Projects.insert({ name, description, completed }).then((projects) => {
+      res.json(projects);
+    });
 });
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, description, completed } = req.body;
 
-router.delete("/:id", async (req, res) => {});
+  if (!name || !description || !completed) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  try {
+    const project = await Projects.get(id);
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    const updatedProject = await Projects.update(id, {
+      name,
+      description,
+      completed,
+    });
+
+    return res.json(updatedProject);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Error trying to update project" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const project = await Projects.get(id);
+    if (!project) {
+      return res.status(404).end();
+    }
+
+    await Projects.remove(id);
+    res.status(204).end();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error deleting project" });
+  }
+});
 
 router.get("/:id/actions", (req, res) => {
   Projects.getProjectActions(req.params.id).then((projectActions) => {
